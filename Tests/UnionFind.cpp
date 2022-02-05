b8eaec1f68b3ca5bc18616cf1d2a9519919bb7da
happypotato1207 <71005325+happypotato1207@users.noreply.github.com>
2022-02-05 11:25:49 +0800

    Create UnionFind.cpp

---
+++ Tests/UnionFind.cpp
@@ -0,0 +1,24 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/unionfind"
+
+#include "../Template/template.cpp"
+#include "../Graph/DSU.hpp"
+
+void init() {
+	// initialize
+
+}
+void solve(int case_no) {
+	// implementation
+	int n, q;
+	cin >> n >> q;
+	DisjointSetUnion DSU(n);
+	while (q--) {
+		int cmd, u, v;
+		cin >> cmd >> u >> v;
+		if (cmd == 0) {
+			DSU.Union(u, v);
+		} else {
+			cout << DSU.CheckConnected(u, v) << endl;
+		}
+	}
+}
