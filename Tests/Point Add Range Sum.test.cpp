f8bd20ac9a0ac4416f85c249f7a176d3665f3c59
happypotato1207 <71005325+happypotato1207@users.noreply.github.com>
2022-02-05 13:29:33 +0800

    Create Point Add Range Sum.test.cpp

---
+++ Tests/Point Add Range Sum.test.cpp
@@ -0,0 +1,31 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/point_add_range_sum"
+
+#include "../Template/template.cpp"
+#include "../Graph/Data_Structures/Segment_Tree.hpp"
+
+void init() {
+	
+}
+void solve(int case_no) {
+	int n, q;
+	cin >> n >> q;
+	vector<Data> v(n);
+	for (int i = 0; i < n; i++) cin >> v[i].val;
+	SegmentTree ST(v);
+	while (q--) {
+		int cmd;
+		cin >> cmd;
+		if (cmd == 0) {
+			Data new_val;
+			int ptr;
+			cin >> ptr >> new_val.val;
+			new_val.val += v[ptr].val;
+			v[ptr] = new_val;
+			ST.Update(ptr, new_val);
+		} else {
+			int x, y;
+			cin >> x >> y;
+			cout << ST.Query(x, y - 1).val << endl;
+		}
+	}
+}
