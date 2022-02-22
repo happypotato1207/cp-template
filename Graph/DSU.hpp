0a2d6ede748baa44a0da423dcb21e409d658a5aa
happypotato1207 <71005325+happypotato1207@users.noreply.github.com>
2022-02-22 21:14:29 +0800

    Update DSU.hpp

--- Graph/DSU.hpp
+++ Graph/DSU.hpp
@@ -1,6 +1,6 @@
 class DisjointSetUnion {
 public:
-	DisjointSetUnion(int n, bool directed_temp = true, bool path_compression_temp = true) {
+	DisjointSetUnion(int n, bool directed_temp = false, bool path_compression_temp = true) {
 		parent.resize(n + 1, -1);
 		rank.resize(n + 1, 1);
 		size.resize(n + 1, 1);
@@ -12,18 +12,19 @@ public:
 		if (path_compression) return parent[u] = FindRoot(parent[u]);
 		else return FindRoot(parent[u]);
 	}
-	void Union(int u, int v) { // if undirected then assign u -> v
+	bool Union(int u, int v) { // if directed then assign u -> v
 		u = FindRoot(u);
 		v = FindRoot(v);
 		if (u != v) {
-			if (directed) {
+			if (!directed) {
 				if (rank[u] < rank[v]) swap(u, v);
 			}
 			parent[u] = v;
 			rank[u] = rank[v] + 1;
 			size[v] += size[u];
+			return true;
 		}
-		return;
+		return false;
 	}
 	bool CheckConnected(int u, int v) {
 		return (FindRoot(u) == FindRoot(v));
