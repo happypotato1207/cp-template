9c2d3b4a4aea59cd722281d7e9913ccead949402
happypotato1207 <71005325+happypotato1207@users.noreply.github.com>
2022-03-08 18:20:49 +0800

    Create Minimum_Spanning_Tree.test.cpp

---
+++ Tests/Minimum_Spanning_Tree.test.cpp
@@ -0,0 +1,21 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/5/GRL/all/GRL_2_A"
+
+#include "../header.cpp"
+#include "../Graph/MST/Kruskal.hpp"
+
+void init() {
+	// initialize
+
+}
+void solve(int case_no) {
+	// implementation
+    int n, m;
+    cin >> n >> m;
+    MST_Kruskal MST(n);
+    for (int i = 0; i < m; i++) {
+        int u, v, w;
+        cin >> u >> v >> w;
+        MST.AddEdge(u, v, w);
+    }
+    cout << MST.ComputeMST() << endl;
+}
