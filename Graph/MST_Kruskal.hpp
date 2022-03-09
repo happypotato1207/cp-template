5c7f5382c2144978e476ca932be635aa66266cb5
happypotato1207 <71005325+happypotato1207@users.noreply.github.com>
2022-03-09 16:50:05 +0800

    Create MST_Kruskal.hpp

---
+++ Graph/MST_Kruskal.hpp
@@ -0,0 +1,38 @@
+// Template: MST - Kruskal's algorithm
+// O(n log n) for every call to ComputeMST()
+// After calling ComputeMST(), the edges will be stored in [res]
+
+#include "DSU.hpp"
+
+struct edge {
+    int u, v;
+    long long w;
+};
+class MST_Kruskal {
+public:
+    vector<edge> res;
+    int n;
+    MST_Kruskal(int sz) {
+        n = sz;
+    }
+    void AddEdge(int u, int v, long long w) {
+        edges.push_back({u, v, w});
+    }
+    long long ComputeMST() {
+        sort(edges.begin(), edges.end(), [](edge &a, edge &b){ return a.w < b.w; });
+        DisjointSetUnion DSU(n);
+        long long tot = 0;
+        int cnt = 0;
+        res.clear();
+        for (edge &cur : edges) {
+            if (DSU.Union(cur.u, cur.v)) {
+                res.push_back(cur);
+                tot += cur.w;
+                cnt++;
+            }
+        }
+        return tot;
+    }
+private:
+    vector<edge> edges;
+};
