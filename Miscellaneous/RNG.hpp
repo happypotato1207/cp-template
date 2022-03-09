3b485683f7fde71c638730d2fb0e9bd2fa8747ff
happypotato1207 <71005325+happypotato1207@users.noreply.github.com>
2022-03-09 16:43:18 +0800

    Create RNG.hpp

---
+++ Miscellaneous/RNG.hpp
@@ -0,0 +1,7 @@
+// Template: Random Number Generator
+// There's basically no way I can validate this (I'll see)
+// Useful for hashing (64-bit hash, check if set is equal in O(1) with failing probability 2^64)
+mt19937_64 rng((int)std::chrono::steady_clock::now().time_since_epoch().count());
+long long rnd(long long l, long long r) { // returns a random number in the range [l, r]
+    return uniform_int_distribution<long long>(l, r)(rng);
+}
