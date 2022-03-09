7d103543db8e5d4bee3635b15fe8312d0772fe7e
happypotato1207 <71005325+happypotato1207@users.noreply.github.com>
2022-03-09 16:48:11 +0800

    Update Interval_Tree.hpp

--- Data_Structures/Interval_Tree.hpp
+++ Data_Structures/Interval_Tree.hpp
@@ -1,6 +1,7 @@
 // Template: Interval Tree (Selective)
 // Doesn't delete range when it overlaps but doesn't completely cover/get covered
 // Deletes the smaller/larger interval depending on [keep_small]
+// Averaged query time O(log n)
 class IntervalTree_Selective {
 public:
     map<int, int> range;
