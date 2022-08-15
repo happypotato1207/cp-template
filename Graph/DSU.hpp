1cac6cde3f1e67f6dd0fb49531544b02c85a9257
happypotato1207 <71005325+happypotato1207@users.noreply.github.com>
2022-08-15 15:34:59 +0800

    Update DSU.hpp

--- Graph/DSU.hpp
+++ Graph/DSU.hpp
@@ -5,49 +5,49 @@
 
 class DisjointSetUnion {
 public:
-	DisjointSetUnion(int n, bool directed_temp = false, bool path_compression_temp = true) {
-		parent.resize(n + 1, -1);
-		rank.resize(n + 1, 1);
-		size.resize(n + 1, 1);
-		directed = directed_temp;
-		path_compression = path_compression_temp;
-	}
-	int FindRoot(int u) {
-		if (parent[u] == -1) return u;
-		if (path_compression) return parent[u] = FindRoot(parent[u]);
-		else return FindRoot(parent[u]);
-	}
-	bool Union(int u, int v) { // if directed then assign u -> v
-		u = FindRoot(u);
-		v = FindRoot(v);
-		if (u != v) {
-			if (!directed) {
-				if (rank[u] < rank[v]) swap(u, v);
-			}
-			parent[u] = v;
-			rank[u] = rank[v] + 1;
-			size[v] += size[u];
-			return true;
-		}
-		return false;
-	}
-	bool CheckConnected(int u, int v) {
-		return (FindRoot(u) == FindRoot(v));
-	}
-	int GetRank(int u) {
-		return rank[u];
-	}
-	int GetSize(int u) {
-		return size[FindRoot(u)];
-	}
-	void Reset() {
-		for (int i = 0; i < parent.size(); i++) {
-			parent[i] = -1;
-			rank[i] = 1;
-			size[i] = 1;
-		}
-	}
+    DisjointSetUnion(int n, bool directed_temp = false, bool path_compression_temp = true) {
+        par.resize(n + 1, -1);
+        rr.resize(n + 1, 1);
+        sz.resize(n + 1, 1);
+        directed = directed_temp;
+        path_compression = path_compression_temp;
+    }
+    int FindRoot(int u) {
+        if (par[u] == -1) return u;
+        if (path_compression) return par[u] = FindRoot(par[u]);
+        else return FindRoot(par[u]);
+    }
+    bool Union(int u, int v) { // if directed then assign u -> v
+        u = FindRoot(u);
+        v = FindRoot(v);
+        if (u != v) {
+            if (!directed) {
+                if (sz[u] < sz[v]) swap(u, v);
+            }
+            par[v] = u;
+            rr[v] = rr[u] + 1;
+            sz[u] += sz[v];
+            return true;
+        }
+        return false;
+    }
+    bool CheckConnected(int u, int v) {
+        return (FindRoot(u) == FindRoot(v));
+    }
+    int GetRank(int u) {
+        return rr[u];
+    }
+    int GetSize(int u) {
+        return sz[FindRoot(u)];
+    }
+    void Reset() {
+        for (int i = 0; i < par.size(); i++) {
+            par[i] = -1;
+            rr[i] = 1;
+            sz[i] = 1;
+        }
+    }
 private:
-	vector<int> parent, rank, size;
-	bool directed, path_compression;
+    vector<int> par, rr, sz;
+    bool directed, path_compression;
 };
