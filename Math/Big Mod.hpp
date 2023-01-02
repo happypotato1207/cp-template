33d5c62c57f87859f6b66c648ea86d60b02b859f
happypotato1207 <71005325+happypotato1207@users.noreply.github.com>
2023-01-02 23:08:53 +0800

    Update Big Mod.hpp

--- Math/Big Mod.hpp
+++ Math/Big Mod.hpp
@@ -1,19 +1,19 @@
 // Template: Big Mod / Mod inverse
 // Finds b^p mod m in O(log p)
 // Mod inverse is application of Fermat's little theorem
-// [MOD] must be a prime
+// [MOD] must be a prime to use modinv()
 
 const int MOD = 1e9 + 7;
-int bigmod(int b, int p) {
-	b %= MOD;
+int bigmod(int b, int p, const int &m = MOD) {
+	b %= m;
 	if (b == 0) return 0;
 	int res = 1;
 	while (p) {
-		if (p & 1) res = (int)(1LL * res * b % MOD);
-		p >>= 1; b = (int)(1LL * b * b % MOD);
+		if (p & 1) res = (int)(1LL * res * b % m);
+		p >>= 1; b = (int)(1LL * b * b % m);
 	}
 	return res;
 }
-int modinv(int x) {
-	return bigmod(x, MOD - 2);
+int modinv(int x, const int &m = MOD) {
+	return bigmod(x, m - 2);
 }
