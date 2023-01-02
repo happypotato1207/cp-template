a5478bba7e9259389932de1480b55402e1933948
happypotato1207 <71005325+happypotato1207@users.noreply.github.com>
2023-01-02 23:16:01 +0800

    Create Power.test.cpp

---
+++ Tests/Power.test.cpp
@@ -0,0 +1,17 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/6/NTL/1/NTL_1_B"
+
+#include "../header.cpp"
+
+const int MOD = 1e9 + 7;
+#include "../Math/Big Mod.hpp"
+
+void init() {
+	// initialize
+
+}
+void solve(int case_no) {
+	// implementation
+	int n, m;
+	cin >> n >> m;
+	cout << bigmod(n, m) << endl;
+}
