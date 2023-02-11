6ba3a0bf2011f66d4b83efd775592c882667d78f
happypotato1207 <71005325+happypotato1207@users.noreply.github.com>
2023-02-11 15:47:13 +0800

    Update RNG.hpp

--- Miscellaneous/RNG.hpp
+++ Miscellaneous/RNG.hpp
@@ -2,7 +2,7 @@
 // There's basically no way I can validate this but this is correct
 // Useful for hashing (64-bit hash, check if set is equal in O(1) with failing probability 2^64)
 
-mt19937_64 rng((int)std::chrono::steady_clock::now().time_since_epoch().count());
+mt19937_64 rng((int)chrono::duration_cast<chrono::milliseconds>(std::chrono::steady_clock::now().time_since_epoch()).count());
 long long rnd(long long l, long long r) { // returns a random number in the range [l, r]
     return uniform_int_distribution<long long>(l, r)(rng);
 }
